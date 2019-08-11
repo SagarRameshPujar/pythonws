bbe0184b7593ca04dc1654b51a0589fc80670e6c
lakshman.avula@outlook.com <lakshman.avula@outlook.com>
2019-08-11 19:43:03 +0530

    Implemented prime logic

--- prime.py
+++ prime.py
@@ -1,1 +1,18 @@
 # Program to check given number is prime or not
+
+num = int(input("Enter the number to check prime :"))
+
+is_prime = True
+
+if num > 1:
+    for i in range(2, num// 2 + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+else:
+    is_prime = False
+
+if is_prime:
+    print(f"Given number :{num} is prime")
+else:
+    print(f"Given number :{num} is not a prime")
